ffeaa079e38bf2c27d8c22ab732264206330111f
Victor Augusto <vaas@icomp.ufam.edu.br>
2022-01-19 20:05:33 -0400

    WIP

--- src/components/CardTask/index.tsx
+++ src/components/CardTask/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {widthPercentageToDP} from 'react-native-responsive-screen';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  bubble: {
+    height: widthPercentageToDP(6.8),
+    width: widthPercentageToDP(6.8),
+    borderRadius: widthPercentageToDP(6.8) / 2,
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+});
 
 const CardTask: React.FC = () => {
   return (
     <View>
-      <TouchableOpacity />
+      <TouchableOpacity style={styles.bubble} />
       <View>
         <Text>Tarefa 01 do dia D</Text>
       </View>
