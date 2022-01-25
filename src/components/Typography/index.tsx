e2553b81c8acfb4696b3e2081f4f5800a0be7d85
Victor Augusto <vaas@icomp.ufam.edu.br>
2022-01-24 20:55:37 -0400

    [Feat] Added loading fonts using expo fonts

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,10 +1,11 @@
+import {useFonts} from 'expo-font';
 import React from 'react';
 import {StyleSheet, Text, TextProps} from 'react-native';
 
 const styles = StyleSheet.create({
-  textRegular: {fontFamily: 'Dongle-Regular'},
-  textBold: {fontFamily: 'Dongle-Bold'},
-  textLight: {fontFamily: 'Dongle-Light'},
+  textRegular: {fontFamily: 'DongleRegular'},
+  textBold: {fontFamily: 'DongleBold'},
+  textLight: {fontFamily: 'DongleLight'},
 });
 
 export type FontWeight = 'regular' | 'light' | 'bold';
@@ -26,7 +27,15 @@ const Typography: React.FC<TypographyProps> = ({
   fontWeight = 'regular',
   ...props
 }) => {
-  return (
+  const [fontsLoaded] = useFonts({
+    DongleRegular: require('../../assets/fonts/Dongle/Dongle-Regular.ttf'),
+    DongleBold: require('../../assets/fonts/Dongle/Dongle-Bold.ttf'),
+    DongleLight: require('../../assets/fonts/Dongle/Dongle-Light.ttf'),
+  });
+
+  return !fontsLoaded ? (
+    <></>
+  ) : (
     <Text {...props} style={[props.style, getFontWeight(fontWeight)]}>
       {children}
     </Text>
