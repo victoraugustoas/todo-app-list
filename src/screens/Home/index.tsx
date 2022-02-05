ccf03943a5e749083cad69feb3f398119c8cb526
Victor Augusto <vaas@icomp.ufam.edu.br>
2022-02-05 01:39:42 -0400

    [Feat] Mark as complete task

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import {
   doc,
   onSnapshot,
   query,
+  serverTimestamp,
+  updateDoc,
 } from 'firebase/firestore';
 import React, {useCallback, useEffect, useState} from 'react';
 import {FlatList, View} from 'react-native';
@@ -16,6 +18,7 @@ interface ITask {
   id: string;
   title: string;
   colorTask: string;
+  selected: boolean;
 }
 
 const HomeScreen: React.FC = () => {
@@ -39,7 +42,6 @@ const HomeScreen: React.FC = () => {
 
   const deleteTask = useCallback(async (task: ITask) => {
     try {
-      // await deleteDoc(doc(fireStore, 'tasks', task.id));
       await deleteDoc(doc(fireStore, 'tasks', task.id));
     } catch (error) {
       console.log('🚀 ~ file: index.tsx ~ line 44 ~ deleteTask ~ error', error);
@@ -47,9 +49,20 @@ const HomeScreen: React.FC = () => {
     }
   }, []);
 
+  const completeTask = useCallback(async (task: ITask) => {
+    try {
+      await updateDoc(doc(fireStore, 'tasks', task.id), {
+        selected: !Boolean(task.selected),
+        completedAt: serverTimestamp(),
+      });
+    } catch (error) {
+    } finally {
+    }
+  }, []);
+
   return (
     <Layout showAddButton>
-      <View style={{marginHorizontal: widthPercentageToDP(3)}}>
+      <View style={{marginHorizontal: widthPercentageToDP(3), flex: 1}}>
         <FlatList
           data={tasks}
           keyExtractor={task => task.id}
@@ -59,13 +72,8 @@ const HomeScreen: React.FC = () => {
                 style={{marginVertical: widthPercentageToDP(1.2)}}
                 key={task.id}
                 title={task.title}
-                selected={selectedTask[task.id]}
-                onPress={() =>
-                  setSelectedTasks(old => ({
-                    ...old,
-                    [task.id]: old[task.id] ? !old[task.id] : true,
-                  }))
-                }
+                selected={task.selected}
+                onPress={() => completeTask(task)}
                 onDimiss={() => deleteTask(task)}
                 colorTask={task.colorTask}
               />
